6c0d849c3ad380f2ece0a164b6d2cffb337824aa
Rohan <35950830+rohan328@users.noreply.github.com>
2022-08-09 20:22:54 -0700

    new updates

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import ParticlesBG from './components/ParticlesBG';
 import reportWebVitals from './reportWebVitals';
-
+import firebase from "./firebase";
 
 ReactDOM.render(
 	<React.StrictMode>
